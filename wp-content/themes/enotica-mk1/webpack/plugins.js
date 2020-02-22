5eac63b5b8832e16b6e3705b35c219005a7f0417
Juan <dientuki@gmail.com>
2020-02-22 15:27:42 -0300

    update js

--- wp-content/themes/enotica-mk1/webpack/plugins.js
+++ wp-content/themes/enotica-mk1/webpack/plugins.js
@@ -1,21 +1,20 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
-const paths = require("./paths");
+const paths = require('./paths');
 const plugins = [];
 
 plugins.push(
-    new CleanWebpackPlugin()
+  new CleanWebpackPlugin()
 );
 
-plugins.push(new MiniCssExtractPlugin({
-    filename:'[name]-[contenthash].css'
-}));
+plugins.push(new MiniCssExtractPlugin({filename: '[name]-[contenthash].css'}));
 
 plugins.push(new WebpackAssetsManifest());
 
 if (process.env.NODE_ENV == 'development') {
   const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
   plugins.push(new HardSourceWebpackPlugin());
 }
 

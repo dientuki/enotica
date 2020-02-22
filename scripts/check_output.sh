6346bba53f9208d597f6bda72865ffce09a4532d
Juan <dientuki@gmail.com>
2020-02-22 15:37:44 -0300

    update test

--- scripts/check_output.sh
+++ scripts/check_output.sh
@@ -2,7 +2,7 @@
 
 echo "Testing the die() on files"
 
-if grep -r --include=\*.php --exclude=class-pclzip.php --exclude=canonical.php --exclude=class-IXR-server.php --exclude=bootstrap.php --extended-regexp "(^|\ )die\(('|\")" ; then exit 1
+if grep -r --include=\*.php --exclude=class-pclzip.php --exclude=canonical.php --exclude=class-IXR-server.php --exclude=bootstrap.php --exclude-dir=smart-slider-3  --extended-regexp "(^|\ )die\(('|\")" ; then exit 1
 fi
 
 echo "Testing the var_dump() on files"

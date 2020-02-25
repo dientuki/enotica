e68e7a0fa122e0ab9130d53b18c88a7a56960f3e
Juan <dientuki@gmail.com>
2020-02-24 23:58:58 -0300

    form

--- wp-content/themes/enotica-mk1/templates/page-tasting.php
+++ wp-content/themes/enotica-mk1/templates/page-tasting.php
@@ -11,14 +11,18 @@ Template Post Type: page
 
 <main class="page-tasting">
   <article>
-    <div class="wrapper">
-      <h1><?php the_title() ?>
-      <div><?php the_content(); ?></div>
+    <div class="wrapper page-tasting__top">
+      <h1 class="page-tasting__title uppercase"><?php the_title() ?></h1>
+      <div class="page-tasting__content"><?php the_content(); ?></div>
     </div>
     
     <?php $tmp = get_field_object( 'cf7' ); ?>
     <?php if ($tmp): ?>
-      <div class="background-bottle"><?php echo do_shortcode($tmp['value']); ?></div>
+      <div class="background-bottle">
+        <div class="wrapper">
+          <?php echo do_shortcode($tmp['value']); ?>
+        </div>
+      </div>
     <?php endif; ?>
   </article>
 </main>

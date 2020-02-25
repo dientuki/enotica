265ba00eecf5940236b9a6fb760a843f0354fc1c
Juan <dientuki@gmail.com>
2020-02-25 00:03:11 -0300

    update plugin translation

--- wp-admin/update-core.php
+++ wp-admin/update-core.php
@@ -532,6 +532,8 @@ function list_translation_updates() {
 		return;
   }
 
+  die(print_r($updates));
+
 	$form_action = 'update-core.php?action=do-translation-upgrade';
 	?>
 	<h2><?php _e( 'Translations' ); ?></h2>

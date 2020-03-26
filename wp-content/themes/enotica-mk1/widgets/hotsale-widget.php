a3a61e59c817975c2954ac40e61e0e6ba91e0c9b
Hugo Silva <hshugoricardosilva@gmail.com>
2020-03-25 21:47:21 -0300

    add the count and icons

--- wp-content/themes/enotica-mk1/widgets/hotsale-widget.php
+++ wp-content/themes/enotica-mk1/widgets/hotsale-widget.php
@@ -2,7 +2,7 @@
 
   <header class="widget-hotsale__header uppercase">
     <div class="wrapper">
-      <h1 class="widget-hotsale__title bold"><span class="widget-hotsale__hot">hot</span> sale</h1>
+      <h1 class="widget-hotsale__title bold"><span class="widget-hotsale__hot">hot</span> <span class="widget-hotsale__sale">sale</span></h1>
       <h2 class="widget-hotsale__subtitle"><?php echo $subtitle; ?></h2>
     </div>
 </header>
@@ -20,20 +20,26 @@
       <ul class="container-products">
         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
           <!-- Template -->
+          <?php $regular_price = get_post_meta( get_the_ID(), '_regular_price', true );?>
+          <?php $price = get_post_meta( get_the_ID(), '_price', true );?>   
+          <?php
+          $price_diference=$regular_price-$price;
+          $percent_discount=$price_diference*100/$regular_price;
+          ?>
           <li class="product type-product status-publish first instock product_cat-hotsale has-post-thumbnail sale shipping-taxable purchasable product-type-simple">
+            
+              <span class="discount">- <?php echo floor($percent_discount);?> %</span>
               <a href="<?php the_permalink(); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-              <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $the_query->post->ID ) );?>
-              <img src="<?php echo $image[0]; ?>" data-id="<?php echo get_the_ID() ?>">
-              <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2>
-              <?php $regular_price = get_post_meta( get_the_ID(), '_regular_price', true );?>
-                <?php $price = get_post_meta( get_the_ID(), '_price', true );?>    
-                <span class="price"><del><span class="woocommerce-Price-amount amount"><?php echo wc_price( $regular_price ); ?> </span></del> <ins><span class="woocommerce-Price-amount amount"><?php echo wc_price( $price ); ?></span></ins></span>
+                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $the_query->post->ID ) );?>
+                <img src="<?php echo $image[0]; ?>" data-id="<?php echo get_the_ID() ?>">
+                <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2> 
+                  <span class="price"><del><span class="woocommerce-Price-amount amount"><?php echo wc_price( $regular_price ); ?> </span></del> <ins><span class="woocommerce-Price-amount amount"><?php echo wc_price( $price ); ?></span></ins></span>
               </a>
-              <a href="?add-to-cart=<?php echo the_ID();?>" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="<?php echo the_ID();?>" data-product_sku="" aria-label="Añade “<?php the_title(); ?>” a tu carrito" rel="nofollow">
-                <div class="icon-cart-button"></div>
-                <div class="text-button">Comprar</div>
-              </a>
-            </li>
+            <a href="?add-to-cart=<?php echo the_ID();?>" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="<?php echo the_ID();?>" data-product_sku="" aria-label="Añade “<?php the_title(); ?>” a tu carrito" rel="nofollow">
+              <div class="icon-cart-button"></div>
+              <div class="text-button">Comprar</div>
+            </a>
+          </li>
           <!-- Fin Template--> 
         <?php endwhile; ?>
       <?php wp_reset_postdata(); ?>

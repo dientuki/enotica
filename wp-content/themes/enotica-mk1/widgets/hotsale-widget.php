d59842e6b8e7561a461a4868f4a3b2fea2136318
Juan <dientuki@gmail.com>
2020-04-23 09:58:20 -0300

    home

--- wp-content/themes/enotica-mk1/widgets/hotsale-widget.php
+++ wp-content/themes/enotica-mk1/widgets/hotsale-widget.php
@@ -9,62 +9,53 @@
   
   <div class="widget-hotsale__container">
     <div class="wrapper">
-<ul class="container-products">
-                  <!-- Template -->
-                       
-                    <li class="product type-product status-publish first instock product_cat-hotsale has-post-thumbnail sale shipping-taxable purchasable product-type-simple">
-            
-              <span class="discount">- 43 %</span>
-                <a href="http://enotica.mateclass.com/index.php/product/vino-especial/" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                    <img width="225" height="225" src="http://enotica.mateclass.com/wp-content/uploads/2020/04/tinto_2.jpeg" data-id="190">
+    <?php
+ 
+      // En 'cat' deberás colocar el ID de la categoría que deseas mostrar.
+      // En 'posts_per_page' deberás colocar la cantidad de posts que deseas mostrar.
+      $the_query = new WP_Query( array( 'product_cat' => 'hotsale', 'posts_per_page' => 12 ) ); ?>
+        
+      <?php if ( $the_query->have_posts() ) : ?>
+      
+      <ul class="container-products">
+        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+          <!-- Template -->
+          <?php $regular_price = get_post_meta( get_the_ID(), '_regular_price', true );?>
+          <?php $price = get_post_meta( get_the_ID(), '_price', true );?>   
+          <?php
+          $price_diference=$regular_price-$price;
+          $percent_discount=$price_diference*100/$regular_price;
+          ?>
+          <li class="product-wrapper type-product status-publish first instock product_cat-hotsale has-post-thumbnail sale shipping-taxable purchasable product-type-simple">
+              <div classs="product">
+                <span class="discount">- <?php echo floor($percent_discount);?> %</span>
+                <a href="<?php the_permalink(); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                  <?php 
+                  /*
+                  $image = wp_get_attachment_image_src( get_post_thumbnail_id( $the_query->post->ID ) );
+                  $image_id=get_post_thumbnail_id( $the_query->post->ID); 
+                  */ 
+                  $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id(), array('225', '225') );
+                  $featured_image_url = $featured_image[0];
+                  $featured_image_width = $featured_image[1];
+                  $featured_image_height = $featured_image[2];
+                  ?>
+                  <img width="225" height="225" src="<?php echo $featured_image[0]; ?>" data-id="<?php echo get_the_ID() ?>">
                 </a>
-                <a href="?add-to-cart=190" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="190" data-product_sku="" aria-label="Añade “VIno Especial” a tu carrito" rel="nofollow">
+                <a href="?add-to-cart=<?php echo the_ID();?>" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="<?php echo the_ID();?>" data-product_sku="" aria-label="Añade “<?php the_title(); ?>” a tu carrito" rel="nofollow">
                   <div class="icon-cart-button"></div>
                   <div class="text-button">Comprar</div>
                 </a>
-              <h2 class="woocommerce-loop-product__title">VIno Especial</h2> 
-              <span class="price"><del><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>300.00</span> </span></del> <ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>170.00</span></span></ins></span>
-              
+                <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2> 
+                <span class="price"><del><span class="woocommerce-Price-amount amount"><?php echo wc_price( $regular_price ); ?> </span></del> <ins><span class="woocommerce-Price-amount amount"><?php echo wc_price( $price ); ?></span></ins></span>
+              </div>              
             
           </li>
           <!-- Fin Template--> 
-                  <!-- Template -->
-                       
-                    <li class="product type-product status-publish first instock product_cat-hotsale has-post-thumbnail sale shipping-taxable purchasable product-type-simple">
-            
-              <span class="discount">- 80 %</span>
-                <a href="http://enotica.mateclass.com/index.php/product/vino-espumante/" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                    <img width="225" height="225" src="http://enotica.mateclass.com/wp-content/uploads/2020/04/blanco_-225x225.jpeg" data-id="187">
-                </a>
-                <a href="?add-to-cart=187" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="187" data-product_sku="" aria-label="Añade “Vino Espumante” a tu carrito" rel="nofollow">
-                  <div class="icon-cart-button"></div>
-                  <div class="text-button">Comprar</div>
-                </a>
-              <h2 class="woocommerce-loop-product__title">Vino Espumante</h2> 
-              <span class="price"><del><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>150.00</span> </span></del> <ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>30.00</span></span></ins></span>
-              
-            
-          </li>
-          <!-- Fin Template--> 
-                  <!-- Template -->
-                       
-                    <li class="product type-product status-publish first instock product_cat-hotsale has-post-thumbnail sale shipping-taxable purchasable product-type-simple">
-            
-              <span class="discount">- 57 %</span>
-                <a href="http://enotica.mateclass.com/index.php/product/tinto-4/" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                    <img width="225" height="225" src="http://enotica.mateclass.com/wp-content/uploads/2020/03/tinto_2.jpeg" data-id="87">
-                </a>
-                <a href="?add-to-cart=87" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="87" data-product_sku="" aria-label="Añade “Tinto #4” a tu carrito" rel="nofollow">
-                  <div class="icon-cart-button"></div>
-                  <div class="text-button">Comprar</div>
-                </a>
-              <h2 class="woocommerce-loop-product__title">Tinto #4</h2> 
-              <span class="price"><del><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>70.00</span> </span></del> <ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span>30.00</span></span></ins></span>
-              
-            
-          </li>
-          <!-- Fin Template--> 
-                          </ul> 
+        <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
+      </ul>      
     </div>
   </div>
 

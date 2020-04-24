a50667e90fde8d5ae28af660075f9ba2d8f657fc
Juan <dientuki@gmail.com>
2020-04-24 01:41:23 -0300

    fix

--- wp-content/themes/enotica-mk1/assets/js/front.js
+++ wp-content/themes/enotica-mk1/assets/js/front.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-new */
+import { killBubling } from './modules/helpers/generic';
 import lzl from './modules/lazyload/native';
 __webpack_public_path__ = `${window.ENO.mainDomain}/dist/`;
 
@@ -14,3 +15,16 @@ if ('IntersectionObserver' in window) {
     module.vanilla(settings_lzl);
   });
 }
+
+document.querySelector('#header-menu-action').addEventListener('click', (e) => {
+  const div = killBubling(e.target, 'DIV');
+
+  div.classList.toggle('active');
+  document.querySelector('div.header__nav').classList.toggle('active');
+});
+
+document.querySelectorAll('a[href="#"]').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.target.parentNode.classList.toggle('active');
+  });
+});

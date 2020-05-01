d1573b14196f86445542587b2df01f447ba96225
Juan "dientuki" Farias <dientuki@gmail.com>
2020-04-30 22:28:58 -0300

    fix

--- wp-content/themes/enotica-mk1/header.php
+++ wp-content/themes/enotica-mk1/header.php
@@ -52,22 +52,22 @@
       </div>
 
     </header>
-    
+
     <?php
         // verify that this is a product category page
-    if ( is_product_category() ){
+    if ( is_product_category() ):
         global $wp_query;
 
         // get the query object
         $cat = $wp_query->get_queried_object();
 
         // get the thumbnail id using the queried category term_id
-        $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); 
+        $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true ); 
 
         // get the image URL
         $image = wp_get_attachment_url( $thumbnail_id ); 
 
         // print the IMG HTML
-        echo "<img src='{$image}' alt='' width='762' height='365' />";
-    }
-    ?>
+        ?>
+        <img src='<?php echo $image; ?>' width='100%' />
+    <?php endif; ?>
